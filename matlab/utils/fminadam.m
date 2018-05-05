861aaa47a1478d21f2c3cb6a19470a1bd5d25011
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-05-05 09:54:10 +0200

    extended interface

--- matlab/utils/fminadam.m
+++ matlab/utils/fminadam.m
@@ -1,8 +1,9 @@
-function [x,f] = fminadam(fun,x0,LB,UB,TolFun)
+function [x,f,xtab,ftab] = fminadam(fun,x0,LB,UB,TolFun,MaxIter)
 
 if nargin < 3; LB = []; end
 if nargin < 4; UB = []; end
 if nargin < 5 || isempty(TolFun); TolFun = 0.001; end
+if nargin < 6 || isempty(MaxIter); MaxIter = 1e4; end
 
 %% Adam with momentum
 fudge_factor = sqrt(eps);
@@ -17,26 +18,26 @@ TolX = 0.001;
 TolX_max = 0.1;
 TolFun_max = TolFun*100;
 
-max_iter = 1e4;
-min_iter = batchsize*2;
+MinIter = batchsize*2;
 
 nvars = numel(x0);
 if isempty(LB); LB = -Inf(nvars,1); end
 if isempty(UB); UB = Inf(nvars,1); end
 
 m = 0; v = 0;
-xtab = zeros(nvars,batchsize*2);
+%xtab = zeros(nvars,batchsize*2);
+xtab = zeros(nvars,MaxIter);
 
 x = x0(:);
-f = NaN(1,max_iter);
+ftab = NaN(1,MaxIter);
 
-for iter = 1:max_iter
+for iter = 1:MaxIter
     idx = mod(iter-1,batchsize*2) + 1;
     isMinibatchEnd = mod(iter,batchsize) == 0;
     
     %if mod(iter,100) == 0; fprintf('%d..',iter); end
     
-    [f(iter),grad] = fun(x);
+    [ftab(iter),grad] = fun(x);
     grad = grad(:);
     
     m = beta1 * m + (1-beta1) * grad;
@@ -50,18 +51,20 @@ for iter = 1:max_iter
     x = x - stepsize * mhat ./(sqrt(vhat) + fudge_factor); % update
     x = min(max(x,LB(:)),UB(:));
 
-    xtab(:,idx) = x;    % Store X
+    % xtab(:,idx) = x;    % Store X
+    xtab(:,iter) = x;   % Store X
         
-    if isMinibatchEnd && iter >= min_iter
+    if isMinibatchEnd && iter >= MinIter
         xxp = linspace(-(batchsize-1)/2,(batchsize-1)/2,batchsize);
-        [p,S] = polyfit(xxp,f(iter-batchsize+1:iter),1);
+        [p,S] = polyfit(xxp,ftab(iter-batchsize+1:iter),1);
         slope = p(1);
         Rinv = inv(S.R); A = (Rinv*Rinv')*S.normr^2/S.df;
         slope_err = sqrt(A(1,1) + TolFun^2);
         slope_err_max = sqrt(A(1,1) + TolFun_max^2);
         
         % Check random walk distance as termination condition
-        dx = sqrt(sum((mean(xtab(:,1:batchsize),2) - mean(xtab(:,batchsize+(1:batchsize)),2)).^2/batchsize,1));
+        %dx = sqrt(sum((mean(xtab(:,1:batchsize),2) - mean(xtab(:,batchsize+(1:batchsize)),2)).^2/batchsize,1));
+        dx = sqrt(sum((mean(xtab(:,iter-batchsize+1:iter),2) - mean(xtab(:,(iter-batchsize+1:iter)-batchsize),2)).^2/batchsize,1));
         
         % Termination conditions
         if ( dx < TolX && abs(slope)<slope_err_max || abs(slope)<slope_err && dx < TolX_max )
@@ -77,12 +80,17 @@ end
 % [x'; mean(xtab,2)']
 % pause
 
-if mod(iter,batchsize*2) == 0
-    x = mean(xtab(:,batchsize+(1:batchsize)),2);
-else
-    x = mean(xtab(:,1:batchsize),2);
-end
-f = mean(f(iter-batchsize+1:iter));
+% if mod(iter,batchsize*2) == 0
+%     x = mean(xtab(:,batchsize+(1:batchsize)),2);
+% else
+%     x = mean(xtab(:,1:batchsize),2);
+% end
+x = mean(xtab(:,iter-batchsize+1:iter),2);
+f = mean(ftab(iter-batchsize+1:iter));
+
+xtab = xtab(:,1:iter);
+ftab = ftab(1:iter);
 
 x = reshape(x,size(x0));
 
+if size(x,2) > 1; xtab = xtab'; end   % Transpose
