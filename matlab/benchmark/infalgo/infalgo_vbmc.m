887260b6ab03f95a5deca3192e0e71b68d56313f
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-05-30 18:32:35 +0200

    modified threshold

--- matlab/benchmark/infalgo/infalgo_vbmc.m
+++ matlab/benchmark/infalgo/infalgo_vbmc.m
@@ -59,7 +59,7 @@ switch algoset
     case {202,'cheapgpentsqrtk'}; algoset = 'cheapgpentsqrtk'; algoptions.CheapGPRetrain = 1; algoptions.NSent = @(K) 100*sqrt(K);
     case {203,'cheapgpentsqrtk2'}; algoset = 'cheapgpentsqrtk2'; algoptions.CheapGPRetrain = 1; algoptions.NSent = @(K) 100*sqrt(K); algoptions.NSentFine = @(K) 2^15*sqrt(K);
     case {204,'cheapgpentsqrtknomid'}; algoset = 'cheapgpentsqrtknomid'; algoptions.CheapGPRetrain = 1; algoptions.NSent = @(K) 100*sqrt(K); algoptions.ELCBOmidpoint = 0;
-    case {205,'gpthreshmid'}; algoset = 'gpthreshmid'; algoptions.GPRetrainThreshold = 10; algoptions.ELCBOmidpoint = 0;
+    case {205,'gpthreshmid'}; algoset = 'gpthreshmid'; algoptions.GPRetrainThreshold = 1; algoptions.ELCBOmidpoint = 0;
     case {206,'gpthreshmidextrak'}; algoset = 'gpthreshmidextrak'; algoptions.GPRetrainThreshold = 3; algoptions.ELCBOmidpoint = 0; algoptions.AdaptiveK = @(K) ceil(K/10); algoptions.KfunMax = @(N) N;
     case {207,'gpthresh'}; algoset = 'gpthresh'; algoptions.GPRetrainThreshold = 1;
     case {208,'cheapgpmid'}; algoset = 'cheapgpmid'; algoptions.CheapGPRetrain = 1; algoptions.ELCBOmidpoint = 0; algoptions.GPRetrainThreshold = Inf;
