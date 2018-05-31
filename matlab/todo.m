215d9a0d227aa5d0022c4b35491ac7f6b68cb864
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-05-31 23:18:20 +0200

    increased widths

--- matlab/todo.m
+++ matlab/todo.m
@@ -1,1 +1,4 @@
-% For rebuttal, possibly rerun BAPE with DRAM and 20k samples
+% - For rebuttal, possibly rerun BAPE with DRAM and 20k samples
+% - Correct BAPE to AGP!
+% - For GP hyperparameter sampling, keep weighted running average of
+%   covariance matrix (weight = 0.5), and use it for random direction slice sampling
