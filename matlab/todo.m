a759cb9a13ab74a90796fb5543d04e738b27a67e
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-06-01 15:53:14 +0200

    changed running hyp cov weight

--- matlab/todo.m
+++ matlab/todo.m
@@ -1,4 +1,3 @@
 % - For rebuttal, possibly rerun BAPE with DRAM and 20k samples
 % - Correct BAPE to AGP!
-% - For GP hyperparameter sampling, keep weighted running average of
-%   covariance matrix (weight = 0.5), and use it for random direction slice sampling
+% - For GP hyperparameter sampling, implement random direction slice sampling
