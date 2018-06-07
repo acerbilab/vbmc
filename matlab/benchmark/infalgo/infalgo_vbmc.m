e3fe6f136d83203f8be466b45ba090626aa00cd8
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-06-07 14:54:50 +0200

    added job

--- matlab/benchmark/infalgo/infalgo_vbmc.m
+++ matlab/benchmark/infalgo/infalgo_vbmc.m
@@ -76,6 +76,7 @@ switch algoset
     case {219,'gpthreshcovsample'}; algoset = 'gpthreshcovsample'; algoptions.GPRetrainThreshold = 1; algoptions.GPSampleWidths = 5; algoptions.HypRunWeight = 0.9; algoptions.WeightedHypCov = 1; algoptions.GPHypSampler = 'covsample'; algoptions.CovSampleThresh = 10;
     case {220,'gpthreshlaplace'}; algoset = 'gpthreshlaplace'; algoptions.GPRetrainThreshold = 1; algoptions.GPSampleWidths = 5; algoptions.HypRunWeight = 0.9; algoptions.WeightedHypCov = 1; algoptions.GPHypSampler = 'laplace';
     case {221,'gpthreshcovsamplezero'}; algoset = 'gpthreshcovsamplezero'; algoptions.GPRetrainThreshold = 1; algoptions.GPSampleWidths = 5; algoptions.HypRunWeight = 0; algoptions.WeightedHypCov = 1; algoptions.GPHypSampler = 'covsample'; algoptions.CovSampleThresh = 10;
+    case {222,'gpthreshcovsample2'}; algoset = 'gpthreshcovsample2'; algoptions.GPRetrainThreshold = 1; algoptions.GPSampleWidths = 5; algoptions.HypRunWeight = 0.9; algoptions.WeightedHypCov = 1; algoptions.GPHypSampler = 'covsample'; algoptions.CovSampleThresh = 10;
                 
     otherwise
         error(['Unknown algorithm setting ''' algoset ''' for algorithm ''' algo '''.']);
