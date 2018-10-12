f8cd801492828627d6045e6708d127468b239056
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-10-12 16:21:41 +0200

    minor changes

--- install.m
+++ install.m
@@ -2,7 +2,7 @@
 %
 % Copyright (c) by Luigi Acerbi 2018
 
-fprintf('Installing VBMC... ');
+fprintf('Installing VBMC...\n');
 
 me = mfilename;                                 % what is my filename
 pathstr = fileparts(which(me));                 % get my location
@@ -15,7 +15,7 @@ catch
 end
 
 if failed_install_flag
-    fprintf('Installation failed: could not save path.\n');
+    fprintf('Installation error: could not save path.\n\n');
     fprintf('You need to manually add VBMC''s installation folder to your MATLAB search path (and save it).\n'); 
     fprintf('See the <a href="https://www.mathworks.com/help/matlab/matlab_env/add-remove-or-reorder-folders-on-the-search-path.html">MATLAB documentation</a> for more information.\n'); 
     fprintf('Note that in Linux systems, e.g. Ubuntu, you need read/write permission to save the MATLAB path (see <a href="https://www.mathworks.com/matlabcentral/answers/95731-why-is-my-modified-path-not-saved-in-matlab">here</a>).\n'); 
