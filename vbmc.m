055977edd1baf699b3ee4cc7779c18da1342d6c1
Luigi Acerbi <luigi.acerbi@gmail.com>
2022-09-06 15:34:10 +0300

    fix: updated VBMC version to 1.0.11

--- vbmc.m
+++ vbmc.m
@@ -1,6 +1,6 @@
 function [vp,elbo,elbo_sd,exitflag,output,samples,optimState,stats,vp_train] = ...
     vbmc(fun,x0,LB,UB,PLB,PUB,options,varargin)
-%VBMC Posterior and model inference via Variational Bayesian Monte Carlo (v1.0.10).
+%VBMC Posterior and model inference via Variational Bayesian Monte Carlo (v1.0.11).
 %   VBMC computes a variational approximation of the full posterior and a 
 %   lower bound on the normalization constant (marginal likelhood or model
 %   evidence) for a provided unnormalized log posterior. As of v1.0, VBMC
@@ -140,8 +140,8 @@ function [vp,elbo,elbo_sd,exitflag,output,samples,optimState,stats,vp_train] = .
 %   Author (copyright): Luigi Acerbi, 2018-2022
 %   e-mail: luigi.acerbi@{helsinki.fi,gmail.com}
 %   URL: http://luigiacerbi.com
-%   Version: 1.0.10
-%   Release date: Jul 23, 2022
+%   Version: 1.0.11
+%   Release date: Sep 06, 2022
 %   Code repository: https://github.com/lacerbi/vbmc
 %--------------------------------------------------------------------------
 
@@ -149,7 +149,7 @@ function [vp,elbo,elbo_sd,exitflag,output,samples,optimState,stats,vp_train] = .
 %% Start timer
 
 t0 = tic;
-vbmc_version = '1.0.10';
+vbmc_version = '1.0.11';
 
 % Check that all VBMC subfolders are on the MATLAB path
 add2path();
